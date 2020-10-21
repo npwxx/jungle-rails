7f6c157bc52e46e22cc6c5af5086a0b357f06e84
Nicole Woodcock <nicole.xox@gmail.com>
2020-10-20 21:08:26 -0700

    add test for adding to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+    # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+  
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+
+  scenario "should see cart size increase in nav bar" do
+    # ACT
+    visit root_path
+
+    expect(page).to have_content('My Cart (0)')
+
+    # VERIFY / DEBUG
+    # puts page.html
+    first('article.product').find_button('Add').click
+  
+    expect(page).to have_content('My Cart (1)')
+    save_screenshot
+  end
+end
